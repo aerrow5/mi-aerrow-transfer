4dff8af21153502f1e33902feb0e775458288259
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-17 21:25:17 +0100

    Build NZBget

--- var/zoneinit/includes/50-nzbget.sh
+++ var/zoneinit/includes/50-nzbget.sh
@@ -1,15 +1,17 @@
-cd
-mkdir build
-cd build
-wget --no-check-certificate https://github.com/nzbget/nzbget/archive/v15.0.zip
-unzip v15.0.zip
-cd nzbget-15.0
+#!/bin/bash
+
+# This script was tested with v.16.4 so get that by default
+version = '16.4'
+
+pushd /tmp
+wget --no-check-certificate https://github.com/nzbget/nzbget/archive/v${version}.zip
+unzip v${version}.zip
+cd nzbget-${version}
 
 ./configure --prefix=/opt/local --with-libxml2-includes=/opt/local/include/libxml2/ --with-libxml2-libraries=/opt/local/lib/ --disable-curses
 make
 make install
 make install-conf
+popd
 
-curl -sk https://gist.githubusercontent.com/baetheus/71533d6358e55342dbb9/raw/nzbget.xml > $HOME/build/nzbget.xml
-svccfg import $HOME/build/nzbget.xml
 svcadm enable nzbget
